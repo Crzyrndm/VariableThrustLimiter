0c28d4a041ed2fc9d93ca553d9ace14163000cb8
Crzyrndm <joshcrawfy@gmail.com>
2015-11-13 10:35:16 +1300

    Linear SRB Thrust limiter

---
+++ VariableThrustLimiter/ModuleSRBThrustLimiterLinear.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+namespace VariableThrustLimiter
+{
+    public class ModuleSRBThrustLimiterLinear : PartModule
+    {
+        [KSPField(isPersistant = true), UI_FloatRange(affectSymCounterparts = UI_Scene.Editor, controlEnabled = true, minValue = 0, maxValue = 100, scene = UI_Scene.Editor, stepIncrement=0.01f)]
+        public float LimiterEnd = 100;
+        float ThrustLimitStart;
+        ModuleEngines SRBModule;
+        PartResource SolidFuelRef;
+        bool bInit = false;
+        public override void OnStart(StartState state)
+        {
+ 	        base.OnStart(state);
+            SRBModule = part.Modules.GetModules<ModuleEngines>().FirstOrDefault(e => e.propellants.FindAll(p => p.name == "SolidFuel").Count > 0);
+            if (SRBModule != null)
+            {
+                ThrustLimitStart = SRBModule.thrustPercentage;
+                SolidFuelRef = part.Resources.list.FirstOrDefault(r => r.resourceName == "SolidFuel");
+                if (SolidFuelRef != null)
+                    bInit = true;
+            }
+        }
+
+        public override void OnFixedUpdate()
+        {
+            base.OnFixedUpdate();
+            if (HighLogic.LoadedSceneIsFlight && !vessel.HoldPhysics && bInit)
+                SRBModule.thrustPercentage = LimiterEnd + (ThrustLimitStart - LimiterEnd) * (float)(SolidFuelRef.amount / SolidFuelRef.maxAmount);
+        }
+    }
+}
---
+++ VariableThrustLimiter/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("VariableThrustLimiter")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("VariableThrustLimiter")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2454d8af-7da5-4fd6-ba13-2eef265b440d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
