835a5507c645f7f58affd90dbc535c5e01337fce
Crzyrndm <joshcrawfy@gmail.com>
2016-04-10 07:49:36 +1200

    Taking advantage of thrustCurve now

---
+++ VariableThrustLimiter/DynamicGimbal.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace VariableThrustLimiter
+{
+    public class DynamicGimbal : ModuleGimbal
+    {
+        ModuleEngines[] engines;
+        ModuleEngines activeEngine;
+        /// <summary>
+        /// remember the default gimbal range
+        /// </summary>
+        float defaultGimbalRange;
+
+        /// <summary>
+        /// Curve describing the relationship between % thrust and % of max gimbal
+        /// </summary>
+        [KSPField]
+        public FloatCurve gimbalPercentage;
+        
+        public override void OnStart(StartState state)
+        {
+            base.OnStart(state);
+
+            engines = part.Modules.GetModules<ModuleEngines>().ToArray();
+            if (part.Modules.GetModules<MultiModeEngine>().Count > 0)
+                StartCoroutine(checkEngineSwitch());
+            if (engines.Length == 0)
+                engines = null;
+            else
+            {
+                activeEngine = engines[0];
+                defaultGimbalRange = gimbalRange;
+            }
+        }
+
+        public IEnumerator checkEngineSwitch()
+        {
+            while (HighLogic.LoadedSceneIsFlight)
+            {
+                yield return new WaitForSeconds(1.0f); // check active engine once per second
+                if (!activeEngine.isOperational) // last active engine is not thrusting
+                {
+                    for (int i = 0; i < engines.Length; ++i)
+                    {
+                        if (engines[i].isOperational)
+                            activeEngine = engines[i];
+                    }
+                }
+            }
+        }
+
+        public override void OnFixedUpdate()
+        {
+            base.OnFixedUpdate();
+            if (engines == null || !activeEngine.isOperational)
+                return;
+            if (!activeEngine.isOperational)
+                gimbalRange = defaultGimbalRange;
+            else
+                gimbalRange = defaultGimbalRange * gimbalPercentage.Evaluate(activeEngine.maxThrust / activeEngine.resultingThrust);
+        }
+    }
+}
--- VariableThrustLimiter/ModuleSRBThrustLimiterLinear.cs
+++ VariableThrustLimiter/ModuleSRBThrustLimiterLinear.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using UnityEngine;
-using System.Collections;
 using System.Linq;
+using UnityEngine;
 
 namespace VariableThrustLimiter
 {
@@ -9,28 +8,37 @@ namespace VariableThrustLimiter
     {
         [KSPField(isPersistant = true), UI_FloatRange(affectSymCounterparts = UI_Scene.Editor, controlEnabled = true, minValue = 0, maxValue = 100, scene = UI_Scene.Editor, stepIncrement=0.01f)]
         public float LimiterEnd = 100;
-        float ThrustLimitStart;
-        ModuleEngines SRBModule;
-        PartResource SolidFuelRef;
-        bool bInit = false;
+
         public override void OnStart(StartState state)
         {
  	        base.OnStart(state);
-            SRBModule = part.Modules.GetModules<ModuleEngines>().FirstOrDefault(e => e.propellants.FindAll(p => p.name == "SolidFuel").Count > 0);
+
+            // only want to modify anything in the flight scene..., for now...
+            if (!HighLogic.LoadedSceneIsFlight)
+                return;
+
+            ModuleEngines SRBModule = part.Modules.GetModules<ModuleEngines>().FirstOrDefault(e => e.propellants.FindAll(p => p.name == "SolidFuel").Count > 0);
+            
+            // We found a SR engine on this part, time to do stuff
             if (SRBModule != null)
             {
-                ThrustLimitStart = SRBModule.thrustPercentage;
-                SolidFuelRef = part.Resources.list.FirstOrDefault(r => r.resourceName == "SolidFuel");
-                if (SolidFuelRef != null)
-                    bInit = true;
+                if (SRBModule.useThrustCurve)
+                {
+                    // if it already has a thrust curve, I want to reshape it to meet my needs
+                    // should really adjust tangents as well. Some other time
+                    float maxTime = Math.Max(SRBModule.thrustCurve.Curve.keys.Max(k => k.time), 0.00001f);
+                    float thrustRatio = (SRBModule.thrustPercentage - LimiterEnd) / (100 * Math.Max(SRBModule.thrustPercentage, 0.00001f));
+                    for (int i = 0; i < SRBModule.thrustCurve.Curve.length; ++i)
+                        SRBModule.thrustCurve.Curve.keys[i].value *= 1 - ((maxTime - SRBModule.thrustCurve.Curve.keys[i].time) / maxTime) * thrustRatio;
+                }
+                else
+                {
+                    // else, I can use it for my own benefit
+                    SRBModule.useThrustCurve = true;
+                    SRBModule.thrustCurve.Add(1f, 1f);
+                    SRBModule.thrustCurve.Add(0f, LimiterEnd / (100f * Math.Max(SRBModule.thrustPercentage, 0.00001f)));
+                }
             }
-        }
-
-        public override void OnFixedUpdate()
-        {
-            base.OnFixedUpdate();
-            if (HighLogic.LoadedSceneIsFlight && !vessel.HoldPhysics && bInit)
-                SRBModule.thrustPercentage = LimiterEnd + (ThrustLimitStart - LimiterEnd) * (float)(SolidFuelRef.amount / SolidFuelRef.maxAmount);
         }
     }
 }
